f5c4ad4ec5d36fafa89af432c99a569f5a6e0e8d
Cristian <cristis991@mail.ru>
2023-02-04 20:32:02 +0200

    go git

--- Fibonnaci_3methods.py
+++ Fibonnaci_3methods.py
@@ -1,25 +1,29 @@
-########## first metod (Simple) ########
+class Fibo:
+    def __init__(self,number=None):
+        self.number = number
 
-num1,num2 = 0,1
-for _ in range(int(input())):
-    num1,num2 = num2,num1+num2
-    print(num1,end=' ')
+    ########## first metod (Simple) ########
+
+    def fibo_simple(self,num1=0,num2=1):
+        for _ in range(self.number):
+            num1,num2 = num2,num1+num2
+        return num1
+
+    ####### second method (Recursion)
+
+    def fibo_recursion (self,num):
+        if  num == 1:
+            return 1
+        elif num == 2 :
+            return 1
+        return self.fibo_recursion(num-1) + self.fibo_recursion(num-2)
+
+    ###### third metod (List_index) #####
+    def fibo_list(self,lst=[1,1]):
+        for _ in range(self.number-2):
+            lst.append(lst[-1]+lst[-2])
+        return lst
+
+operator = Fibo(int(input('Put you number : ')))
 
 
-####### second method (Recursion)
-
-def fibo (num):
-    if num == 1:
-        return 1
-    elif num == 2 :
-        return 1
-    return fibo(num-1) + fibo(num-2)
-print(fibo(int(input())))
-
-###### third metod (List_index) #####
-def fibo_list(num,lst=[1,1]):
-    for _ in range(num-2):
-        lst.append(lst[-1]*lst[-2])
-    return lst
-
-print(fibo(int(input())))
