fa6f43e5816cad1db6428fd86048771e3d9c28ff
Cristian <cristis991@mail.ru>
2023-02-04 20:14:16 +0200

    фибо 3 метода

---
+++ Fibonnaci_3methods.py
@@ -0,0 +1,26 @@
+########## first metod (Simple) ########
+
+num1,num2 = 0,1
+
+for _ in range(int(input())):
+    num1,num2 = num2,num1+num2
+    print(num1,end=' ')
+
+
+####### second method (Recusion)
+
+def fibo (num):
+    if num == 1:
+        return 1
+    elif num == 2 :
+        return 1
+    return fibo(num-1) + fibo(num-2)
+print(fibo(int(input())))
+
+###### third metod (List_index) #####
+def fibo_list(num,lst=[1,1]):
+    for _ in range(num-2):
+        lst.append(lst[-1]*lst[-2])
+    return lst
+
+print(fibo(6))
