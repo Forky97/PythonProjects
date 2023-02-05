f5f7e2706bd8d517d549eb6ba090952c79452ad1
Cristian <cristis991@mail.ru>
2023-02-05 21:11:57 +0200

    tests

---
+++ Django_tests.py
@@ -0,0 +1,14 @@
+######################## Documentation django tests #########################
+from django.test import TestCase
+
+from .models import Post
+
+class PostModelTest (TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='just a text')
+
+    def test_text_context(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = post.text
+        self.assertEqual(expected_object_name,'just a text')
